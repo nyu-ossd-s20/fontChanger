d81d9a555b4be6dc76f0eba6cb82f99bd51d9ed4
Quang Luong <quangio@outlook.com>
2020-02-09 11:16:23 -0500

    refactor, add temporary icon

--- popup/sendFont.js
+++ popup/sendFont.js
@@ -1,15 +1,15 @@
-function updateFont(tabs){
+function updateFont(tabs) {
   const form = document.getElementById('font');
-  form.addEventListener('submit', on);
-  var list = document.getElementById("dropdown");
-  var font;
-  function on(tabs){
+  form.addEventListener('submit', onSubmit);
+  const list = document.getElementById("dropdown");
+  let font;
+  function onSubmit(tabs) {
     font = list.options[list.selectedIndex].value;
     browser.tabs.query({active: true, currentWindow: true})
         .then(change)
   }
 
-  function change(tabs){
+  function change(tabs) {
     browser.tabs.sendMessage(tabs[0].id, {
       command: font,
     });
@@ -18,5 +18,4 @@ function updateFont(tabs){
   console.error(font);
 }
 
-browser.tabs.executeScript({file: "/content_scripts/change.js"})
-.then(updateFont);
+browser.tabs.executeScript({file: "/content_scripts/change.js"}).then(updateFont);
