cc5f4c8b2c6415fda1481d3fa61ec959f89e6e88
mstillman9 <43043050+mstillman9@users.noreply.github.com>
2020-02-11 23:02:11 -0500

    a much easier way of doing this

--- content_scripts/change.js
+++ content_scripts/change.js
@@ -1,6 +1,3 @@
 browser.runtime.onMessage.addListener((message) => {
-    const tags = document.getElementsByTagName("*");
-    for (let tag of tags) {
-      tag.style.setProperty("font-family", message.command, "important");
-    }
+      document.body.style.setProperty("font-family", message.command, "important");
 });
